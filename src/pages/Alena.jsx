3c8f287b0b9c9442ea3c25edad7b2a0fdf91bace
tr43om <tr43om@gmail.com>
2022-03-08 22:30:39 +0600

    aboa

--- src/pages/Alena.jsx
+++ src/pages/Alena.jsx
@@ -1,7 +1,9 @@
 import { Input } from "../components/Input";
+import { Question } from "../components/Question";
 export const Alena = () => {
   return (
     <>
+      <Question />
       <Input label="АТВИЧАЙ, АЛЕНА" name="Алена" />
     </>
   );
